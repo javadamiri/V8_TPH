3cf2b2456f40a77fc5ecbdbccb37d6ee2735b64d
Javad Amiri <javad@fox.moma>
2020-09-25 13:26:11 +1000

    seg fault in Builtins_RecordWrite

--- src/builtins/builtins-internal-gen.cc
+++ src/builtins/builtins-internal-gen.cc
@@ -407,6 +407,10 @@ TF_BUILTIN(RecordWrite, RecordWriteCodeStubAssembler) {
   Label incremental_wb(this);
   Label exit(this);
 
+#ifdef V8_ENABLE_THIRD_PARTY_HEAP
+  Goto(&exit);
+#else
+
   TNode<Smi> remembered_set =
       UncheckedCast<Smi>(Parameter(Descriptor::kRememberedSet));
   Branch(ShouldEmitRememberSet(remembered_set), &generational_wb,
@@ -498,7 +502,7 @@ TF_BUILTIN(RecordWrite, RecordWriteCodeStubAssembler) {
           function, object, slot, isolate_constant, fp_mode, &exit);
     }
   }
-
+#endif
   BIND(&exit);
   IncrementCounter(isolate()->counters()->write_barriers(), 1);
   Return(TrueConstant());
